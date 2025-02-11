59544c21697588d10b687ecfafb8cd7b47ecad21
funfish22 <j26391442@gmail.com>
2025-02-11 22:25:04 +0800

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let itemCounter = 0;
 let isEditMode = false;
 let currentTag = 'travelTw';
-let ver = '20250211_2'
+let ver = '20250211_3'
 
 const initialItems = {
     travelTw: ["證件 (身分證、健保卡、駕照)", "衣物 (衣服、褲子、內褲、襪子)", "衣物收納袋或乾淨塑膠袋", "洗漱用品 (牙膏、牙刷、洗面乳、漱口水)", "毛巾", "控油洗髮精", "衛生紙", "保養品", "雨傘", "保溫瓶", "原子筆", "手機", "平板電腦", "充電器 (相機、手機、平版充電器)", "行動電源", "相機 (注意是否有記憶卡和電池)", "現金和信用卡", "藥物", "防蚊液 (防小黑蚊)"],
@@ -10,8 +10,16 @@ const initialItems = {
 };
 
 document.addEventListener('DOMContentLoaded', function() {
-    loadList();
     writeVer();
+    const localVer = localStorage.getItem('ver')
+    if(localVer === null) localStorage.setItem('ver', ver)
+    if(localVer !== ver) {
+        localStorage.setItem('ver', ver)
+        localStorage.removeItem('travelTwList')
+        localStorage.removeItem('travelGlobalList')
+        localStorage.removeItem('businessList')
+    }
+    loadList();
 
     const newItemInput = document.getElementById("newItem");
     const addButton = document.getElementById("addButton");
@@ -30,15 +38,6 @@ document.addEventListener('DOMContentLoaded', function() {
         event.preventDefault();
         addItem();
     });
-
-    const localVer = localStorage.getItem('ver')
-    if(localVer === null) localStorage.setItem('ver', ver)
-    if(localVer !== ver) {
-        localStorage.setItem('ver', ver)
-        localStorage.removeItem('travelTwList')
-        localStorage.removeItem('travelGlobalList')
-        localStorage.removeItem('businessList')
-    }
 });
 
 function fixVh() {
