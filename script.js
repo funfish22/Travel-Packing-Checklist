83e5a4f881be7deddb0b1702c38293be99bb8cbf
funfish22 <j26391442@gmail.com>
2025-02-11 22:16:43 +0800

    update

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 let itemCounter = 0;
 let isEditMode = false;
 let currentTag = 'travelTw';
+let ver = '20250211_1'
 
 const initialItems = {
-    travelTw: ["證件 (身分證、健保卡、駕照)", "衣物 (衣服、褲子、內褲、襪子)", "衣物收納袋", "洗漱用品 (牙膏、牙刷、洗面乳、漱口水)", "毛巾", "控油洗髮精", "保養品", "手機", "平板電腦", "充電器 (相機、手機、平版充電器)", "行動電源", "相機 (注意是否有記憶卡和電池)", "現金和信用卡", "藥物", "防蚊液 (防小黑蚊)"],
-    travelGlobal: ["護照", "機票", "旅館預訂確認", "衣物 (衣服、褲子、內褲、襪子)", "洗漱用品 (牙膏、牙刷、洗面乳、漱口水)", "充電器 (相機、手機、平版充電器)", "相機 (注意是否有記憶卡和電池)", "現金和信用卡"],
-    business: ["筆記本電腦", "名片", "文件夾", "商務服裝", "充電器", "筆", "記事本", "商務手機"]
+    travelTw: ["證件 (身分證、健保卡、駕照)", "衣物 (衣服、褲子、內褲、襪子)", "衣物收納袋或乾淨塑膠袋", "洗漱用品 (牙膏、牙刷、洗面乳、漱口水)", "毛巾", "控油洗髮精", "衛生紙", "保養品", "雨傘", "保溫瓶", "原子筆", "手機", "平板電腦", "充電器 (相機、手機、平版充電器)", "行動電源", "相機 (注意是否有記憶卡和電池)", "現金和信用卡", "藥物", "防蚊液 (防小黑蚊)"],
+    travelGlobal: ["護照", "機票", "旅館預訂確認", "衣物 (衣服、褲子、內褲、襪子)", "衣物收納袋或乾淨塑膠袋", "洗漱用品 (牙膏、牙刷、洗面乳、漱口水)", "毛巾", "控油洗髮精", "衛生紙", "保養品", "雨傘", "保溫瓶", "原子筆", "漫遊sim卡", "手機", "平板電腦", "充電器 (相機、手機、平版充電器)", "行動電源", "相機 (注意是否有記憶卡和電池)", "現金和信用卡", "拖鞋", "藥物", "防蚊液 (防小黑蚊)"],
+    business: ["筆記本電腦", "名片", "文件夾", "商務服裝", "充電器", "原子筆", "記事本", "商務手機"]
 };
 
 document.addEventListener('DOMContentLoaded', function() {
     loadList();
+    writeVer();
 
     const newItemInput = document.getElementById("newItem");
     const addButton = document.getElementById("addButton");
@@ -29,11 +31,14 @@ document.addEventListener('DOMContentLoaded', function() {
         addItem();
     });
 
-    
-});
-
-window.addEventListener('load', (event) => {
-    // fixVh()
+    const localVer = localStorage.getItem('ver')
+    if(localVer === null) localStorage.setItem('ver', ver)
+    if(localVer !== ver) {
+        localStorage.setItem('ver', ver)
+        localStorage.removeItem('travelTwList')
+        localStorage.removeItem('travelGlobalList')
+        localStorage.removeItem('businessList')
+    }
 });
 
 function fixVh() {
@@ -206,4 +211,9 @@ function uncheckAll() {
         label.classList.remove("checked"); // 移除 checked 樣式
     });
     saveList(); // 保存狀態
+}
+
+function writeVer() {
+    const verDom = document.querySelector('.ver');
+    verDom.textContent = `版本: ${ver}`
 }
